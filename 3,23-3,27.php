35731222ce07650b8df4e4713101b5ea1cef2e8d
Skamky_H <moskam@tuta.io>
2023-09-07 17:21:25 +0500

    3.23-3.27

---
+++ 3,23-3,27.php
@@ -0,0 +1,38 @@
+<?php
+//листинг 3.23
+
+class  AdressManager
+{
+	private $addresses = ["209.131.36.159","216.58.213.174"];
+	//листинг 3.26
+	/**Вывести список адресов.
+	 * Если переменная $resolve содержит значения true, то адрес преобразуеться в эквивалентное имя хоста
+	 * @param $resolve Boolean Преоброзовать адрес?
+	 * @return void
+	 */
+	public function outputAddresses($resolve)
+	{
+		//листинг 3.27 (использовалась до версии php7
+		if (!is_bool($resolve)){
+			//принять решительные меры}
+		}
+		// листинг  3.25 (нежелательный код)
+		if (is_string($resolve)){
+			$resolve = (preg_match("/^(false|no|off)$/i",$resolve))?false:true;
+		}
+		foreach ($this->addresses as $address)
+		{
+			print $address;
+			if($resolve)
+			{
+				print "(".gethostbyaddr($address).")";
+			}
+			print  "\n";
+		}
+
+	}
+}
+//листинг 3.24
+$settings = simplexml_load_file(__DIR__."/resolve.xml");
+$manager = new AdressManager();
+$manager->outputAddresses((string)$settings->resolvedomains);
